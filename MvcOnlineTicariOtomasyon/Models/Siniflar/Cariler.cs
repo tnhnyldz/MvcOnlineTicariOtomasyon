956cd8dc09dc77742fc989101eda8b53b4e78a16
tnhnyldz <tunahanyildiz1560@gmail.com>
2022-03-31 13:41:08 +0300

    Classes and restrictions added

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariID { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariSoyad { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(13)]
+        public string CarsiSehir { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+        public SatisHaraket SatisHaraket { get; set; }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/SatisHaraket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHaraket
+    {
+        [Key]
+        public int SatisID { get; set; }
+        //Urun
+        //Cari
+        //Personel
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public ICollection<Urun> Uruns { get; set; }
+        public ICollection<Cariler> Carilers { get; set; }
+        public ICollection<Personel> Personels { get; set; }
+
+    }
+}
