ed7746608ca28943c746806e3b90cc7bf8106013
tnhnyldz <tunahanyildiz1560@gmail.com>
2022-04-18 14:49:52 +0300

    Faturakalem section

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
@@ -26,7 +26,9 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [StringLength(60)]
         public string VergiDairesi { get; set; }
 
-        public DateTime Saat { get; set; }
+        [Column(TypeName = "char")]
+        [StringLength(5)]
+        public string Saat { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
@@ -35,6 +37,8 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string TeslimEden { get; set; }
+
+        public decimal Toplam { get; set; }
 
         public ICollection<FaturaKalem> FaturaKalems { get; set; }
     }
