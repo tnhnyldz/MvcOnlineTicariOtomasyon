7ad95adf2a627f23c90c2d46f16b8bef1f466a3c
tnhnyldz <tunahanyildiz1560@gmail.com>
2022-04-25 15:32:11 +0300

    error

---
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return PartialView();
+
+        }
+
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler p)
+        {
+            var bilgiler = c.Carilers.FirstOrDefault
+                (x=>x.CariMail==p.CariMail && x.CariSifre==p.CariSifre);
+
+            if (bilgiler!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return PartialView("Index","CariPanel");
+            }
+            else
+            {
+                return View("Index","Login");
+            }
+            
+        }
+        public PartialViewResult Partial2()
+        {
+            return PartialView();
+        }
+
+    }
+}
