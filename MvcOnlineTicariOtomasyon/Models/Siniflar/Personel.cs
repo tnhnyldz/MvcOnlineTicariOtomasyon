5a13d8e26f9fc3812fac85e5bf7da79d3216145d
tnhnyldz <tunahanyildiz1560@gmail.com>
2022-04-25 17:33:39 +0300

    Fixx

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
@@ -12,10 +12,12 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [Key]
         public int PersonelID { get; set; }
 
+        [Display(Name ="Personel Adı")]
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string PersonelAd { get; set; }
 
+        [Display(Name = "Personel Soyadı")]
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string PersonelSoyad { get; set; }
