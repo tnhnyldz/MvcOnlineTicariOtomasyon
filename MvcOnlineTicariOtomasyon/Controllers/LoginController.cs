bbef9143eb50b53aea0a89ad4c8d6ed936e43335
tnhnyldz <tunahanyildiz1560@gmail.com>
2022-04-26 11:15:49 +0300

    Auth Section 19/139

--- MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,23 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public PartialViewResult partial1()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return PartialView();
         }
     }
 }
--- MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -28,6 +28,10 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [StringLength(50)]
         public string CariMail { get; set; }
 
+        [Column(TypeName = "Varchar")]
+        [StringLength(20)]
+        public string CariSifre { get; set; }
+
         public bool Durum { get; set; }
         public ICollection<SatisHaraket> SatisHarakets { get; set; }
     }
